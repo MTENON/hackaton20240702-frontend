051586b2d15c95168e59e05636484072d200d0c3
Maxime Tenon <maxime.tenon@gmail.com>
2024-07-07 16:37:31 +0200

    Added page change. Added cart functions.

--- scripts/script_cart.js
+++ scripts/script_cart.js
@@ -3,20 +3,46 @@ const link = "http://localhost:3000";
 
 console.log('Script_cart loaded');
 
-function addListeners() {
-    for (let index = 0; index < document.querySelectorAll('.item').length; index++) {
-        document.querySelectorAll('.item')[index].addEventListener('click', function () {
+function hideText() {
 
-        })
-    }
+}
+
+function addListeners() {
 
     document.querySelector('#purchase').addEventListener('click', function () {
 
         fetch(`${link}/myCart/bookCart/`).then(response => response.json())
             .then((data) => {
-                console.log(data);
+                // console.log(data);
+                location.assign('./page_booking.html')
             })
     })
+
+    //En appuyant sur la croix retire la ligne clickée
+    //Retire l'objet de la DB MyCart
+    //Met à jour le total
+    for (let i = 0; i < document.querySelectorAll('.cartDelete').length; i++) {
+        document.querySelectorAll('.cartDelete')[i].addEventListener('click', function () {
+
+            const query = {
+                departure: this.parentNode.firstElementChild.firstElementChild.textContent,
+                arrival: this.parentNode.firstElementChild.lastElementChild.textContent,
+                price: this.previousElementSibling.firstElementChild.textContent,
+            }
+
+            this.parentNode.remove();
+
+            fetch(`${link}/myCart/`, {
+                method: "DELETE",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(query)
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+        })
+    }
+
 }
 
 function getCart() {
@@ -26,7 +52,16 @@ function getCart() {
         .then((data) => {
 
             console.log(data);
+
             document.querySelector('.totalprice').textContent = `${data.price}€`
+
+            //Faire disparaitre le texte qui sert à rien
+            if (data.result.length === 0) {
+                document.querySelector('#mycart2').style = "display: none";
+                return;
+            }
+
+            document.querySelector('#notickets').style = "display: none";
 
             for (let i = 0; i < data.result.length; i++) {
                 document.querySelector('#mycart').innerHTML += `
@@ -37,14 +72,18 @@ function getCart() {
                         <p class="cart_arrival">${data.result[i].arrival}</p>
                     </div>
                     <p class="cart_hour">${data.hour[i]}</p>
-                    <p class="cart_price">${data.result[i].price}€</p>
-                    <button class="cart_delete">X</button>
+                    <div class="cart_price_container">
+                        <p class="cart_price">${data.result[i].price}</p>
+                        <p>€</p>
+                    </div>
+                    <button class="cartDelete">X</button>
                 </div>
                 `;
             }
             addListeners();
         })
 }
+
 
 //Initialisation
 getCart();
